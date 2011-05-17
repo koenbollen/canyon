aad2993055dcb653806b6a715d0dd18416ae2566
Koen Bollen <meneer@koenbollen.nl>
2011-05-17 13:22:42 +0200

    Redesigned the HUD, reusing the PlayerMode.

--- Canyon/Canyon/HUD/Display.cs
+++ Canyon/Canyon/HUD/Display.cs
@@ -1,33 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using Canyon.Entities;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Canyon.Entities;
 
 namespace Canyon.HUD
 {
-    public enum HUDState
-    {
-        FirstPerson = 1,
-        ThirdPerson = 2,
-    }
     public class Display : DrawableGameComponent
     {
-        public HUDState State { get; set; }
-
         internal SpriteBatch Batch { get; private set; }
         internal SpriteFont Font { get; private set; }
-        private List<HUDItem> HUDItems;
-        private Player player;
+        internal Player Player { get; private set; }
+
+        private List<Item> Items;
 
         public Display(Game game, Player p)
             :base(game)
         {
-            this.player = p;
-            this.State = HUDState.ThirdPerson;
-            HUDItems = new List<HUDItem>();
+            this.Player = p;
+            Items = new List<Item>();
         }
 
         protected override void LoadContent()
@@ -35,28 +25,29 @@ namespace Canyon.HUD
             Batch = new SpriteBatch(GraphicsDevice);
             Font = Game.Content.Load<SpriteFont>("Fonts/default");
             AddItems();
-            for (int i = 0; i < HUDItems.Count; i++)
-                HUDItems[i].Initialize();
+            for (int i = 0; i < Items.Count; i++)
+                Items[i].Initialize();
             base.LoadContent();
         }
 
         private void AddItems()
         {
-            this.HUDItems.Add( new Speed(Game, this, player) );
+            this.Items.Add(new Speed(Game, this));
+            this.Items.Add(new Crosshair(Game, this));
         }
 
         public override void Update(GameTime gameTime)
         {
-            for (int i = 0; i < HUDItems.Count; i++)
-                    HUDItems[i].Update(gameTime);
+            for (int i = 0; i < Items.Count; i++)
+                    Items[i].Update(gameTime);
             base.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
-            for (int i = 0; i < HUDItems.Count; i++)
-                if (((int)this.State & (int)HUDItems[i].Type) != 0)
-                    HUDItems[i].Draw(gameTime);
+            for (int i = 0; i < Items.Count; i++)
+                if (Items[i].ShowInMode(Player.CurrentMode) )
+                    Items[i].Draw(gameTime);
             base.Draw(gameTime);
         }
     }
